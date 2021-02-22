6f1bf9a2d283ab485472cab55eed7fb48033c454
Santosh <kasantosh@yahoo.com>
2021-02-22 12:51:14 -0500

    Created basic routes

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const users = require('./routes/users');
+const contacts = require('./routes/contacts');
+const auth = require('./routes/auth');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World from Server'));
+app.get('/', (req, res) => res.status(200).json({
+  status: 'Success',
+  message: 'Succeeded'
+})
+
+);
+
+app.use('/api/users', users);
+app.use('/api/contacts', contacts);
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 
