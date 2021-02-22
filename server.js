93f13f5061d24a9aa4de500396865acbe6078001
Santosh <kasantosh@yahoo.com>
2021-02-22 12:04:10 -0500

    Initial commit

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
 const express = require("express");
-const connectDB = require("./config/db");
 
 const app = express();
 
-connectDB();
-
-//Init middleware
-app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API..." })
-);
-
-//Define Routes
-
-app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/contacts", require("./routes/contacts"));
+app.get('/', (req, res) => res.send('Hello World from Server'));
 
 const PORT = process.env.PORT || 5000;
 
